d17d70acf210be9f82e6fc0771b9cfc001e19c65
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-01-24 18:00:20 -0800

    Fixed frequency bug

--- histogram_generator.py
+++ histogram_generator.py
@@ -2,41 +2,33 @@
 PUNCTUATION = '''!()-[]{};:'"\,<>./?@#$%^&*_~”“\n'''
 
 def histogram(filename='test_text.txt', type='d'):
+    words = []
+    with open(filename, 'r') as text:
+            for raw_line in text:
+                line = ''
+                for char in raw_line:
+                    if char not in PUNCTUATION:
+                        line += char
+                words += line.lower().split()
     if type == 'd':
         histogram = {}
-        words = []
-        with open(filename, 'r') as text:
-            for raw_line in text:
-                line = ''
-                for char in raw_line:
-                    if char not in PUNCTUATION:
-                        line += char
-                words += line.lower().split()
-            for word in words:
-                if word in histogram.keys():
-                    histogram[word] += 1
-                else:
-                    histogram.update({word: 1})
+        for word in words:
+            if word in histogram.keys():
+                histogram[word] += 1
+            else:
+                histogram.update({word: 1})
     elif type == 't':
         histogram = []
-        words = []
-        with open(filename, 'r') as text:
-            for raw_line in text:
-                line = ''
-                for char in raw_line:
-                    if char not in PUNCTUATION:
-                        line += char
-                words += line.lower().split()
-            words.sort()
-            index = -1
-            for word in words:
-                if len(histogram) != 0 and word == histogram[index][0]:
-                    count = histogram[index][1] + 1
-                    del histogram[-1]
-                    histogram.append((word, count))
-                else:
-                    histogram.append((word, 1))
-                    index += 1
+        words.sort()
+        index = -1
+        for word in words:
+            if len(histogram) != 0 and word == histogram[index][0]:
+                count = histogram[index][1] + 1
+                del histogram[-1]
+                histogram.append((word, count))
+            else:
+                histogram.append((word, 1))
+                index += 1
     return histogram
 
 
@@ -45,11 +37,15 @@ def unique_words(histogram):
 
 def frequency(histogram, word):
     word = word.lower()
-    if word in histogram:
-        return histogram[word]
-    else:
-        return 0
-
+    if isinstance(histogram, dict):
+        if word in histogram:
+            return histogram[word]
+    elif isinstance(histogram, list):
+        for group in histogram:
+            if group[0] == word:
+                return group[1]
+    return 0
+    
 if __name__ == '__main__':
     import time
     import math
@@ -57,10 +53,10 @@ if __name__ == '__main__':
     d_histogram = histogram(type='d')
     end = time.time()
     print('Time to create dict histogram', round((end - start) * 1000, 2), 'ms')
-    print('Creating touple histogram...')
     start = time.time()
     t_histogram = histogram(type='t')
     end = time.time()
     print('Time to create touple histogram', round((end - start) * 1000, 2), 'ms')
-    print('Unique words in histogram:', unique_words(d_histogram))
-    print('Frequency of the in histogram', frequency(d_histogram, 'the'))
+    print('Unique words:', unique_words(d_histogram))
+    print('"the" :', frequency(d_histogram, 'the'))
+
