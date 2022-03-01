a5263daa8fbe7abe43324650bd0793fd69da9f41
Dylan Player <dylanplayer@Dylans-MacBook-Pro.local>
2022-02-28 20:50:03 -0800

    Made much faster

--- Code/cleanup.py
+++ Code/cleanup.py
@@ -6,8 +6,8 @@ def cleaner(filename):
   tweets = []
   with open(filename, 'r') as data:
     for tweet in data:
-      temp = re.sub("@[A-Za-z0-9]+","",tweet)
-      temp = re.sub("#[A-Za-z0-9_]+","", temp)
+      temp = re.sub(r"@[A-Za-z0-9]+","",tweet)
+      temp = re.sub(r"#[A-Za-z0-9_]+","", temp)
       temp = re.sub(r"http\S+", "", temp)
       temp = re.sub(r"www.\S+", "", temp)
       tweets.append(temp)
@@ -16,11 +16,11 @@ def cleaner(filename):
       data.write(tweet)
   words = []
   with open(filename, 'r') as text:
-          for raw_line in text:
-              line = ''
-              for char in raw_line:
-                    line += char
-              words += line.lower().split()
+    for raw_line in text:
+        line = ''
+        for char in raw_line:
+              line += char
+        words += line.lower().split()
   with open(filename, 'w') as data:
     for word in words:
       if d.check(word):
